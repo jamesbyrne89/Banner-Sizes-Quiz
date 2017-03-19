2473ca393326677a0e0a6ffc114e0e029d801d44
jamesbyrne89 <mail@jamestbyrne.com>
2017-03-19 22:34:35 +0000

    Adding blurred background

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,21 +1,21 @@
-
-
 const banners = document.getElementsByClassName('banner');
 const wrapper = document.getElementsByClassName('wrapper');
+const blurredBackground = document.getElementById('popup-wrap')
+const body = document.getElementById('body');
 
+(function addClickEvents() {
+	for (var i = 0; i < banners.length; i++) {
+		banners[i].addEventListener('click', function() {
+			wrapper[0].classList.toggle('flex');
+this.classList.toggle('clicked-banner');
+blurredBackground.classList.toggle('blurred-background')
+console.log(this === banners[0])
+			for (var i = 0; i < banners.length; i++) {
+				if (banners[i] !== this) {			
+					banners[i].classList.remove('clicked-banner');
 
-(function addClickEvents(){
-	for (var i = 0; i < banners.length; i++){
-banners[i].addEventListener('click', function(){
-	this.classList.toggle('clicked-banner');
-	wrapper[0].classList.toggle('flex');
-	for (var i = 0; i < banners.length; i++){
-		if (banners[i] !== this){
-			banners[i].classList.toggle('blur');
-		}
+				}
+			}
+		})
 	}
-})
-}
 })();
-
-

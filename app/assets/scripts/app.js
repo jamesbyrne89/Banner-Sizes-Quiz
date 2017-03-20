8404e0ae64917816e7bee32a6d62ae1971c619e1
jamesbyrne89 <mail@jamestbyrne.com>
2017-03-20 22:51:48 +0000

    Blurred background attempt pure CSS

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -8,8 +8,6 @@ const body = document.getElementById('body');
 		banners[i].addEventListener('click', function() {
 			wrapper[0].classList.toggle('flex');
 this.classList.toggle('clicked-banner');
-blurredBackground.classList.toggle('blurred-background')
-console.log(this === banners[0])
 			for (var i = 0; i < banners.length; i++) {
 				if (banners[i] !== this) {			
 					banners[i].classList.remove('clicked-banner');
